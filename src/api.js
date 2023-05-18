ff74cb48dc4c30f03109deaa1c0b37e5954af08c
dastan4477 <58164311+dastan4477@users.noreply.github.com>
2023-05-18 22:39:53 +0200

    Forgot to save it

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fd840eeb22msh40d9604a0d81ef5p11d0b1jsnea5dc52ddcdf',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+export const WEATHER_API_KEY = "757ffd893f6198ca85ce59b418cc1968"
